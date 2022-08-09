dfa79c09deab0148e7a007973b4f6aef78801d7c
AmirWijaya <109485015+AmirWijaya@users.noreply.github.com>
2022-08-09 15:41:31 +0700

    Initialize dependabot/npm_and_yarn/react/tar-4.4.19

---
+++ vue/src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../components/About.vue";
+import Profile from "../components/Profile.vue";
+import Login from "../components/Login.vue";
+import Bloog from "../components/Bloog.vue";
+import Bloog from "../components/Footer.vue";
+
+
+Vue.use(VueRouter);
